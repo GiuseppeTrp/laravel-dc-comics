05ac368cab3cbb28926c3db120ea6c470381547b
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-04-11 17:37:29 +0200

    end milestone 1

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,9 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Guest\PageController;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', [PageController::class, 'index'])->name('home');
+Route::resource('comic', ComicController::class);
 
