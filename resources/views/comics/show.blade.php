566ab602f81b2fef5fc84f07ec6d7f713af5e355
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-04-13 01:06:55 +0200

    Delete aDD, end CRUD

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -29,12 +29,53 @@
             <a href="{{route('comic.index', $comic->id)}}" class="btn btn-outline-light text-uppercase fw-bold text-black">Back</a>
             <a href="#" class="btn btn-outline-success text-uppercase fw-bold text-warning">Buy</a>
             <a href="{{route('comic.edit', $comic->id)}}" class="btn btn-outline-warning text-uppercase fw-bold text-success">Edit</a>
+            <button type="button" class="btn btn-outline-danger text-warning text-uppercase fw-bold" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              Delete
+           </button>
   
           </div>
+         
+    
+          
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered ">
+              <div class="modal-content">
+
+                <div class="modal-header">
+                  <h1 class="modal-title fs-5" id="exampleModalLabel">Delete comic</h1>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+                  Are you sure? Deleting "{{$comic->title}}"
+                </div>
 
 
-        </div>
-    </div>
+                <div class="modal-footer">
+
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <form action="{{route('comic.destroy', $comic->id)}}" method="POST">
+                        @csrf
+                        @method("DELETE")
+                        
+                       
+                        <button class="btn btn-danger">Elimina</button>
+                    </form>
+
+                </div>
+
+              </div>
+            </div>
+
+
+                </div>
+
+
+
+
+              </div>
+          </div>
 
    
       
